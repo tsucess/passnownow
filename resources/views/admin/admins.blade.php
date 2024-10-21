ccd2f7e64268f6c627e9e9ff0a84f392ac74e9e4
tsuccess <taofeeq.muhammad22@gmail.com>
2024-10-21 15:33:54 +0100

    Revert "Topic edit feature updated:"
    This reverts commit 052999de5caf76932df34698503136b5095a7666.

--- resources/views/admin/admins.blade.php
+++ resources/views/admin/admins.blade.php
@@ -33,15 +33,6 @@
         @php
             $sn= 0;
         @endphp
-<<<<<<< HEAD
-=======
-
-    {{$sn= 0;}}
-    {{-- <php if (isset($output)) {?> --}}
->>>>>>> parent of cd22f4e (project updated)
-=======
-    {{-- <php if (isset($output)) {?> --}}
->>>>>>> parent of 052999d (Topic edit feature updated)
     <div class="table-responsive mb-5 pb-5">
         <table id="admin-table" class="table custom-table mb-5 pb-5">
             <thead class="bg-info">
