6af59227af9d5bd0c3dab0c46a4013edb85ad0a0
tsuccess <taofeeq.muhammad22@gmail.com>
2024-12-06 12:11:27 +0100

    Update suscription feature

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -6,8 +6,11 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use App\Models\Transaction;
+use App\Models\Admin;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use DateTime;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -27,9 +30,21 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request): RedirectResponse
     {
-        // dd($request);
+        
         $request->authenticate();
         $request->session()->regenerate();
+
+        $userID = Auth::user()->unique_id;
+       
+        $subHistory = Transaction::where('user_unique_id', $userID)->where('payment_status', 'success')->get();
+        $subExpiry = Transaction::where('user_unique_id', $userID)->where('payment_status', 'success')->latest('updated_at')->limit(1)->get();
+
+        $exp = date_create($subExpiry[0]->expiry_date);
+        $exp_d = date_format($exp, 'Y-m-d');
+        $now = date('Y-m-d');
+        if ($now > $exp_d) {
+            Admin::where('unique_id', $userID)->where('role', 'user')->update(['status'=> 0]);
+        }
 
         return redirect()->intended(route('dashboard', absolute: false));
     }
