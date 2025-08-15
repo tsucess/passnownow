0bf39a80a8bcba27946c2c4561e6b8a94785d8a3
tsuccess <taofeeq.muhammad22@gmail.com>
2025-08-15 12:55:30 +0100

    Email notification i

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -1,48 +1,49 @@
 <x-mail::message>
-    {{-- Greeting --}}
-    @if (!empty($greeting))
-        # {{ $greeting }}
-    @else
-        @if ($level === 'error')
-            # Whoops!
-        @else
-            # Hi {{ $user->name ?? 'there' }},
-        @endif
-    @endif
+{{-- Greeting --}}
+@if (! empty($greeting))
+# {{ $greeting }}
+@else
+@if ($level === 'error')
+# Whoops!
+@else
+# Hi {{ $user->name ?? 'there' }},
+@endif
+@endif
 
-    
-    {{-- Intro Lines --}}
-    @foreach ($introLines as $line)
-        {{ $line }}
-    @endforeach
+{{-- Intro Lines --}}
+@foreach ($introLines as $line)
+{{ $line }}
 
-    {{-- Action Button --}}
-    @isset($actionText)
-        <?php
-        $color = match ($level) {
-            'success', 'error' => $level,
-            default => 'primary',
-        };
-        ?>
-        <x-mail::button :url="$actionUrl" :color="$color">
-            {{ $actionText }}
-        </x-mail::button>
-    @endisset
+@endforeach
 
-    {{-- Outro Lines --}}
-    @foreach ($outroLines as $line)
-        {{ $line }}
-    @endforeach
+{{-- Action Button --}}
+@isset($actionText)
+<?php
+    $color = match ($level) {
+        'success', 'error' => $level,
+        default => 'primary',
+    };
+?>
+<x-mail::button :url="$actionUrl" :color="$color">
+{{ $actionText }}
+</x-mail::button>
+@endisset
 
-    {{-- Closing --}}
-    Thanks,
-    **{{ config('app.name') }}** Team
+{{-- Outro Lines --}}
+@foreach ($outroLines as $line)
+{{ $line }}
 
-    {{-- Subcopy --}}
-    @isset($actionText)
-        <x-slot:subcopy>
-            If you have trouble clicking **{{ $actionText }}**, copy and paste this link in your browser:
-            <span class="break-all">{{ $displayableActionUrl }}</span>
-        </x-slot:subcopy>
-    @endisset
+@endforeach
+
+{{-- Closing --}}
+Thanks,  
+**{{ config('app.name') }}** Team
+
+{{-- Subcopy --}}
+@isset($actionText)
+<x-slot:subcopy>
+If you have trouble clicking **{{ $actionText }}**, copy and paste this link in your browser:  
+<span class="break-all">{{ $displayableActionUrl }}</span>
+</x-slot:subcopy>
+@endisset
 </x-mail::message>
