ee2db0b29cd6735b099de31fb87cc1b7fde2f9f7
tsuccess <taofeeq.muhammad22@gmail.com>
2025-08-15 09:33:48 +0100

    if parameter updated

--- resources/views/admin/exam_taken.blade.php
+++ resources/views/admin/exam_taken.blade.php
@@ -23,7 +23,7 @@
                             <div class="card">
 
                                 <div class="card-body">
-                                    @if ($results)
+                                    @if ($results->isNotEmpty())
                                         <div class="table-responsive">
                                             <table class="table table-striped table-bordered table-hover datatable">
                                                 <thead>
