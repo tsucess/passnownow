e9c4c8c0ba012616b541e4741ec60791edf7f340
tsuccess <taofeeq.muhammad22@gmail.com>
2025-08-15 09:24:54 +0100

    No exams notification and email logo fixed

--- resources/views/admin/exam_taken.blade.php
+++ resources/views/admin/exam_taken.blade.php
@@ -23,48 +23,61 @@
                             <div class="card">
 
                                 <div class="card-body">
-                                    <div class="table-responsive">
-                                        <table class="table table-striped table-bordered table-hover datatable">
-                                            <thead>
-                                                <tr>
-                                                    <th>#</th>
-                                                    <th>Exam title</th>
-                                                    <th>Exam Date</th>
-                                                    <th>Status</th>
-                                                    <th>Result</th>
-                                                    <th>Actions</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                @foreach ($results as $attempt)
+                                    @if ($results)
+                                        <div class="table-responsive">
+                                            <table class="table table-striped table-bordered table-hover datatable">
+                                                <thead>
                                                     <tr>
-                                                        <td>{{ $loop->iteration }}</td>
-                                                        <td>{{ $attempt->exam->title ?? 'Unknown Exam' }}</td>
-                                                        <td>
-                                                            <p style="width:10rem">
-                                                                {{ \Carbon\Carbon::parse($attempt->updated_at)->format('d M Y H:i') }}
-                                                            </p>
-                                                        </td>
-                                                        <td>Completed</td>
-                                                        <td>
-                                                            <a href="{{ url('/view_result', ['data' => $attempt]) }}"
-                                                                class="btn btn-secondary btn-sm" style="width:6rem">View
-                                                                Result</a>
-                                                        </td>
-                                                        <td>
-                                                            <a href="{{ url('/view_answer', ['data' => $attempt]) }}"
-                                                                class="btn btn-primary btn-sm" style="width:7rem">View
-                                                                Answers</a>
-                                                        </td>
+                                                        <th>#</th>
+                                                        <th>Exam title</th>
+                                                        <th>Exam Date</th>
+                                                        <th>Status</th>
+                                                        <th>Result</th>
+                                                        <th>Actions</th>
                                                     </tr>
-                                                @endforeach
+                                                </thead>
+                                                <tbody>
+                                                    @foreach ($results as $attempt)
+                                                        <tr>
+                                                            <td>{{ $loop->iteration }}</td>
+                                                            <td>{{ $attempt->exam->title ?? 'Unknown Exam' }}</td>
+                                                            <td>
+                                                                <p style="width:10rem">
+                                                                    {{ \Carbon\Carbon::parse($attempt->updated_at)->format('d M Y H:i') }}
+                                                                </p>
+                                                            </td>
+                                                            <td>Completed</td>
+                                                            <td>
+                                                                <a href="{{ url('/view_result', ['data' => $attempt]) }}"
+                                                                    class="btn btn-secondary btn-sm" style="width:6rem">View
+                                                                    Result</a>
+                                                            </td>
+                                                            <td>
+                                                                <a href="{{ url('/view_answer', ['data' => $attempt]) }}"
+                                                                    class="btn btn-primary btn-sm" style="width:7rem">View
+                                                                    Answers</a>
+                                                            </td>
+                                                        </tr>
+                                                    @endforeach
 
-                                            </tbody>
-                                            <tfoot>
+                                                </tbody>
+                                                <tfoot>
 
-                                            </tfoot>
-                                        </table>
-                                    </div>
+                                                </tfoot>
+                                            </table>
+                                        </div>
+                                    @else
+
+                                    @else
+                                        {{-- <p class="text-center">No Exams attempted yet!</p> --}}
+                                        <div class="alert alert-info text-center">
+                                            <h4>No Exams Found</h4>
+                                            <p>You haven’t attempted any exams yet. Once you do, your results will appear
+                                                here.</p>
+                                            <a href="/adexams" class="btn btn-primary">Browse Available Exams</a>
+                                        </div>
+                                    @endif
+                                    @endif
                                 </div>
                                 <!-- /.card-body -->
                             </div>
